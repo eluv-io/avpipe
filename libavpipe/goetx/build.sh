b3aa470b3b8da1001630ed219ddc09c72477ae5c
Reza Mir <rmir@Reza-MacBook-Pro-10.local>
2019-01-11 20:22:45 -0500

    Fix the build error on mac for goetx

--- libavpipe/goetx/build.sh
+++ libavpipe/goetx/build.sh
@@ -1,1 +1,2 @@
-CGO_CFLAGS="-I../include -I../../utils/include -I${ELV_TOOLCHAIN_DIST_PLATFORM}/include" CGO_LDFLAGS="-L${ELV_TOOLCHAIN_DIST_PLATFORM}/lib -L../../utils/lib -L../lib -lavpipe -lavcodec -lavformat -lavfilter -lavdevice -lswresample -lswscale -lavutil -lpostproc -lutils -lm -ldl -lpthread" go build -v
+TOP_DIR=`pwd`
+CGO_CFLAGS="-I../include -I../../utils/include -I${ELV_TOOLCHAIN_DIST_PLATFORM}/include" CGO_LDFLAGS="-L${ELV_TOOLCHAIN_DIST_PLATFORM}/lib -L${TOP_DIR}/../../utils/lib -L${TOP_DIR}/../lib -lavpipe -lavcodec -lavformat -lavfilter -lavdevice -lswresample -lswscale -lavutil -lpostproc -lutils -lm -ldl -lpthread" go build -v -x
