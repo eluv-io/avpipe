4069412431190d4d9d25b8e49894fc3327c426ee
Nate Armstrong <nate.armstrong@eluv.io>
2025-01-30 11:58:11 -0800

    Add script to init env based on new toolchain

---
+++ init-local.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export FFMPEG_DIST="$HOME/.local"
+export SRT_DIST="$HOME/.local/bin"
+export PKG_CONFIG_PATH="$HOME/.local/lib/pkgconfig"
+echo FFMPEG_DIST=$FFMPEG_DIST
+echo SRT_DIST=$SRT_DIST
+echo PKG_CONFIG_PATH=$PKG_CONFIG_PATH
