16dd53bf0608e1c83385754749fe0a26d618c62b
Nate Armstrong <nate.armstrong@eluv.io>
2025-03-06 11:10:05 -0800

    Allow secondary dispatch of warn/error avp logs

--- avpipe_log.go
+++ avpipe_log.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"encoding/binary"
 	"encoding/hex"
+	"fmt"
+	"strings"
 	"sync"
 
 	"github.com/modern-go/gls"
@@ -33,11 +35,13 @@ func (l *logWrapper) Info(msg string, fields ...interface{}) {
 }
 
 func (l *logWrapper) Warn(msg string, fields ...interface{}) {
+	dispatchToChannelIfPresent("WARN", msg, fields...)
 	fields = append(fields, logHandleIfKnown()...)
 	l.log.Warn(msg, fields...)
 }
 
 func (l *logWrapper) Error(msg string, fields ...interface{}) {
+	dispatchToChannelIfPresent("ERROR", msg, fields...)
 	fields = append(fields, logHandleIfKnown()...)
 	l.log.Error(msg, fields...)
 }
@@ -50,13 +54,40 @@ func (l *logWrapper) Fatal(msg string, fields ...interface{}) {
 var log = logWrapper{log: elog.Get("/avpipe")}
 
 var gidHandleMap sync.Map = sync.Map{}
+var gidChanMap sync.Map = sync.Map{}
+
+var handleChanMap map[int32]chan string = make(map[int32]chan string)
+var handleChanMapMu sync.Mutex
 
 func AssociateGIDWithHandle(handle int32) {
-	gidHandleMap.Store(gls.GoID(), handle)
+	gid := gls.GoID()
+	gidHandleMap.Store(gid, handle)
+	if ch, ok := gidChanMap.LoadAndDelete(gid); ok {
+		handleChanMapMu.Lock()
+		handleChanMap[handle] = ch.(chan string)
+		handleChanMapMu.Unlock()
+	}
 }
 
 func DissociateGIDWithHandle() {
 	gidHandleMap.Delete(gls.GoID())
+}
+
+// RegisterWarnErrChanForHandle registers a channel to send error logs to for a given handle.
+// If handle is nil, then channel will be registered with a handle that is created on this
+// goroutine.
+func RegisterWarnErrChanForHandle(handle *int32, errChan chan string) {
+	if handle == nil {
+		gidChanMap.Store(gls.GoID(), errChan)
+		return
+	}
+
+	handleChanMapMu.Lock()
+	if _, ok := handleChanMap[*handle]; ok {
+		log.Warn("RegisterErrorChanForHandle: handle already registered with channel", "handle", *handle)
+	}
+	handleChanMap[*handle] = errChan
+	handleChanMapMu.Unlock()
 }
 
 func GIDHandle() (int32, bool) {
@@ -75,4 +106,20 @@ func logHandleIfKnown() []interface{} {
 		return []interface{}{"avp", hex.EncodeToString(buf.Bytes())}
 	}
 	return nil
+}
+
+func dispatchToChannelIfPresent(level string, msg string, fields ...interface{}) {
+	if handle, ok := GIDHandle(); ok {
+		if ch, ok := handleChanMap[handle]; ok {
+			//space-combine fields
+			strs := []string{level, msg}
+			for _, field := range fields {
+				strs = append(strs, fmt.Sprint(field))
+			}
+			select {
+			case ch <- strings.Join(strs, " "):
+			default:
+			}
+		}
+	}
 }
