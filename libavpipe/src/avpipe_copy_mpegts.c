aadb99ba389fd50fb0cf43ebe9831afc24220143
Nate Armstrong <nate.armstrong@eluv.io>
2025-03-14 14:45:00 -0700

    Fix NPE?

--- libavpipe/src/avpipe_copy_mpegts.c
+++ libavpipe/src/avpipe_copy_mpegts.c
@@ -181,10 +181,10 @@ copy_mpegts_prepare_audio_encoder(
 
         /* Open audio encoder codec */
         if (avcodec_open2(encoder_context->codec_context[output_stream_index], encoder_context->codec[output_stream_index], NULL) < 0) {
-            char *codec_str = NULL;
-            char *ctx_str = NULL;
-            av_opt_serialize(encoder_context->codec_context[output_stream_index]->av_class, 0, 0, &codec_str, '=', ':');
-            av_opt_serialize(encoder_context->codec_context[output_stream_index]->av_class, AV_OPT_FLAG_ENCODING_PARAM, 0, &ctx_str, '=', ':');
+            char *codec_str;
+            char *ctx_str;
+            av_opt_serialize(encoder_context->codec_context[output_stream_index], 0, 0, &codec_str, '=', ':');
+            av_opt_serialize(encoder_context->codec_context[output_stream_index], AV_OPT_FLAG_ENCODING_PARAM, 0, &ctx_str, '=', ':');
 
             elv_dbg("Could not open encoder for audio, stream_index=%d", stream_index);
             elv_dbg("codec=%s,\n\n ctx=%s", codec_str, ctx_str);
