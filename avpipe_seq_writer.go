c00707b6b545e9b181fb576b0876a4c4c5a43500
Nate Armstrong <nate.armstrong@eluv.io>
2025-07-25 21:56:14 -0700

    Add sequential writer

---
+++ avpipe_seq_writer.go
@@ -0,0 +1,114 @@
+package avpipe
+
+// #cgo pkg-config: libavcodec
+// #cgo pkg-config: libavfilter
+// #cgo pkg-config: libavformat
+// #cgo pkg-config: libavutil
+// #cgo pkg-config: libswresample
+// #cgo pkg-config: libavresample
+// #cgo pkg-config: libavdevice
+// #cgo pkg-config: libswscale
+// #cgo pkg-config: libavutil
+// #cgo pkg-config: libpostproc
+// #cgo netint pkg-config: xcoder
+// #cgo pkg-config: srt
+// #cgo CFLAGS: -I${SRCDIR}/include
+// #cgo CFLAGS: -I${SRCDIR}/libavpipe/include
+// #cgo CFLAGS: -I${SRCDIR}/utils/include
+// #cgo LDFLAGS: -L${SRCDIR}
+// #cgo linux LDFLAGS: -Wl,-rpath,$ORIGIN/../lib
+// #include "avpipe.h"
+import "C"
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"sync"
+
+	"github.com/eluv-io/avpipe/goavpipe"
+)
+
+var outputStillOpenErr = errors.New("cannot open new sequential output, previous still open")
+var outputClosedErr = errors.New("output is closed, cannot write to it")
+var alreadyClosedErr = errors.New("output already closed")
+
+type SequentialOpener interface {
+	OpenNext() (io.WriteCloser, error)
+}
+
+func NewAVPipeSequentialOutWriter(inFd int64, streamIndex int, streamType goavpipe.AVType, firstSegIdx int) SequentialOpener {
+	return &avpipeSequentialOutHandler{
+		inFd:         inFd,
+		streamIndex:  streamIndex,
+		streamType:   streamType,
+		nextSegIndex: firstSegIdx,
+	}
+}
+
+type avpipeSequentialOutHandler struct {
+	mu sync.Mutex
+
+	//inFd is the identifier assigned to the input when it was opened. In some places this is called
+	//'handler'.
+	inFd        int64
+	streamIndex int
+	streamType  goavpipe.AVType
+
+	nextSegIndex int
+	// outFd is the identifier assigned to the output. If nil, it means there are no open outputs.
+	outFd *int64
+}
+
+func (h *avpipeSequentialOutHandler) OpenNext() (io.WriteCloser, error) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	if h.outFd != nil {
+		return nil, outputStillOpenErr
+	}
+
+	outFd := AVPipeOpenOutputGo(h.inFd, h.streamIndex, h.nextSegIndex, 0, h.streamType)
+	if outFd < 0 {
+		return nil, fmt.Errorf("failed to open next output for input fd %d", h.inFd)
+	}
+	h.outFd = &outFd
+
+	// Increment the segment index for the next output.
+	h.nextSegIndex++
+
+	return h, nil
+}
+
+func (h *avpipeSequentialOutHandler) Write(p []byte) (n int, err error) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	if h.outFd == nil {
+		return 0, outputClosedErr
+	}
+
+	n = AVPipeWriteOutputGo(h.inFd, *h.outFd, p)
+	if n < 0 {
+		return 0, fmt.Errorf("failed to write to output fd %d", *h.outFd)
+	}
+
+	return n, nil
+}
+
+func (h *avpipeSequentialOutHandler) Close() error {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	if h.outFd == nil {
+		return outputClosedErr
+	}
+
+	rv := AVPipeCloseOutputGo(h.inFd, *h.outFd)
+	if rv < 0 {
+		return fmt.Errorf("failed to close output fd %d", *h.outFd)
+	}
+
+	h.outFd = nil
+	return nil
+}
